fb2ee5070c839b72ab186fc2a429f7552f658f1d
jonathanvictorica <jonathan@techforb.com>
2023-05-07 21:39:47 -0300

    export data

--- CheckAgro/api-check/src/main/java/com/jmg/checkagro/check/controller/mapper/CheckMapper.java
+++ CheckAgro/api-check/src/main/java/com/jmg/checkagro/check/controller/mapper/CheckMapper.java
@@ -3,7 +3,11 @@ package com.jmg.checkagro.check.controller.mapper;
 
 import com.jmg.checkagro.check.controller.request.CheckRequest;
 import com.jmg.checkagro.check.controller.response.CheckResponse;
+import com.jmg.checkagro.check.controller.response.CustomerCheckLimitResponse;
+import com.jmg.checkagro.check.controller.response.ProviderCheckLimitResponse;
 import com.jmg.checkagro.check.model.Check;
+import com.jmg.checkagro.check.model.CustomerCheckLimit;
+import com.jmg.checkagro.check.model.ProviderCheckLimit;
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
@@ -15,4 +19,8 @@ public interface CheckMapper {
     Check toCheck(CheckRequest request);
 
     CheckResponse toCheckResponse(Check entity);
+
+    CustomerCheckLimitResponse toCustomerCheckLimitResponse(CustomerCheckLimit entity);
+
+    ProviderCheckLimitResponse toProviderCheckLimitResponse(ProviderCheckLimit entity);
 }
